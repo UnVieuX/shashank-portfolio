1a415dcde6bac2323333b3f891b8068b74ff27e2
Shashank Singh <57018679+UnVieuX@users.noreply.github.com>
2025-03-25 19:03:48 +0530

    Update layout.jsx

--- shashank-portfolio/app/layout.jsx
+++ shashank-portfolio/app/layout.jsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetBrainsMono.variable}>
+        <Analytics />
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
