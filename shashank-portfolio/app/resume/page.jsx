d0cdc14b303585f1ad3883ac061cb51f2f456898
Shashank Singh <57018679+UnVieuX@users.noreply.github.com>
2025-03-25 19:32:11 +0530

    Update page.jsx

--- shashank-portfolio/app/resume/page.jsx
+++ shashank-portfolio/app/resume/page.jsx
@@ -162,9 +162,9 @@ const Resume = () => {
               <TabsContent value="experience" className="w-full">
                 <div className="flex flex-col gap-[30px] text-center xl:text-left">
                   <h3 className="text-4xl font-bold">{experience.title}</h3>
-{/*                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                     {experience.description}
-                  </p> */}
+                  </p>
                   <ScrollArea className="h-[400px]">
                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                       {experience.items.map((item, index) => {
@@ -221,9 +221,9 @@ const Resume = () => {
                 <div className="flex flex-col gap-[30px]">
                   <div className="flex flex-col gap-[30px] text-center xl:text-left">
                     <h3 className="text-4xl font-bold">{skills.title}</h3>
-                    <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+{/*                     <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                       {skills.description}
-                    </p>
+                    </p> */}
                   </div>
                   <ScrollArea className="h-[400px]">
                     <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 xl:grid-cols-5 xl:gap-2 gap-4">
